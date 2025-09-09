5f95c7b5e4b0c3b7aa627b70cd69ab88d85cd98f
ajaysinghrajput9370 <ajaysinghrajput9370@gmail.com>
2025-09-09 17:40:27 +0530

    Update app.py
    aassd

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def create_order():
         return jsonify({"error": "Payment gateway not configured"}), 500
 
     plan = request.json.get("plan", "basic")
-    amount_map = {"basic": 100, "standard": 350000, "premium": 600000}  # in paise
+    amount_map = {"basic": 300000, "standard": 350000, "premium": 600000}  # in paise
 
     if plan not in amount_map:
         return jsonify({"error": "Invalid plan"}), 400
